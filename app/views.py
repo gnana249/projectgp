832f9e9778bba39020d829966022e2b37667fce4
prakash <saignana545@gmail.com>
2023-11-10 11:49:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def  sai(request):
+    return HttpResponse(' <h1><marquee>hello gnana no work for you </h1></marquee>')
